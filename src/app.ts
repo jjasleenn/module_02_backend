bfd71a00ea77b5257afe67fa17fe662658f59b6b
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-09-30 22:06:27 -0500

    set up the files and integrate the morgan

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+ const app = express();
+
+// Importing morgan
+import morgan from "morgan";
+
+// Use morgan for HTTP request logging
+app.use(morgan("combined"));
+
+export default app;
