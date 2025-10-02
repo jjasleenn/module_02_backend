96b8e0e913261d6941277e210795b5e24d8423a9
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-10-01 22:26:30 -0500

    wrote the test for create the employee - missing params

--- test/employeeroute.test.ts
+++ test/employeeroute.test.ts
@@ -4,7 +4,7 @@ import app from "../src/app";
 describe("Employee API Endpoints", () => {
   let employeeId: number;
 
-  // 1️⃣ Create Employee - success
+  // 1 Create Employee - success
   it("should create an employee successfully", async () => {
     const res = await request(app).post("/api/v1/employees").send({
       name: "John Doe",
@@ -19,4 +19,14 @@ describe("Employee API Endpoints", () => {
     expect(res.body).toHaveProperty("id");
     expect(res.body.name).toBe("John Doe");
     employeeId = res.body.id;
+  });
+
+   // 2️ Create Employee - missing params  
+  it("should return 400 when creating an employee with missing fields", async () => {
+    const res = await request(app).post("/api/v1/employees").send({
+      name: "Jane Doe",
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("message", "Missing required fields");
   });})
