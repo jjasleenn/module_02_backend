c91211e104fe07c1815bedf82569978fe5ebe554
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-10-01 21:52:15 -0500

    setup the express server with branch routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+
 import employeeRoutes from "./api/v1/routes/employeeroutes";
+import branchRoutes from "./api/v1/routes/branchroutes";
 
 const app = express();
 
@@ -15,5 +17,7 @@ app.get("/health", (req, res) => {
 
 // API routes
 app.use("/api/v1/employees", employeeRoutes);
+app.use("/api/v1/branches", branchRoutes);
 
 export default app;
+
